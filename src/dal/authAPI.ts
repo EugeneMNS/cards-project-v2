9b0f9d662e3b43e2763e69c72c8e9d70db538de2
EugeneMNS <evgen.str4@gmail.com>
2022-07-23 18:28:32 +0300

    login types

--- src/dal/authAPI.ts
+++ src/dal/authAPI.ts
@@ -1,7 +1,25 @@
 import {instance, UserDomainType} from "./api";
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
+import {LoginParamsType, ResponseType} from "./types";
 
+
+export const authAPI = {
+    login(data: LoginParamsType) {
+        const promise = instance.post<ResponseType<{userId?: number}>>('auth/login', data);
+        return promise;
+    },
+    logout() {
+        const promise = instance.delete<ResponseType<{userId?: number}>>('auth/login');
+        return promise;
+    },
+    me() {
+        const promise =  instance.get<ResponseType<{id: number; email: string; login: string}>>('auth/me');
+        return promise
+    }
+}
+
+/*
 export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
     const { data } = await axios.post('/auth/login', params);
     return data;
@@ -15,4 +33,4 @@ export const fetchRegister = createAsyncThunk('auth/fetchRegister', async (param
 export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
     const { data } = await axios.get('/auth/me');
     return data;
-});
+});*/
---
+++ src/dal/types.ts
@@ -0,0 +1,13 @@
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe: boolean
+    captcha?: string
+}
+export type FieldErrorType = { field: string; error: string }
+export type ResponseType<D = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors?: Array<FieldErrorType>
+    data: D
+}
