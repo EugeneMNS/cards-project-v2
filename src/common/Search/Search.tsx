856ba12fd9bffc3d59ebbfcddd8682895d38a66b
EugeneMNS <evgen.str4@gmail.com>
2022-08-02 15:24:48 +0300

    feat: added debounce for search

--- src/common/Search/Search.tsx
+++ src/common/Search/Search.tsx
@@ -2,17 +2,18 @@ import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from 'react';
 import s from './Search.module.scss';
 import {useAppDispatch} from "../../redux/store";
 import {setSearchPacksValue} from "../../features/PacksList/packsSlice";
+import useDebounce from "./useDebounce";
 
 
 type SearchPropsType = {
     value: string
     onKeyUpHandler: (value: string) => void
 }
-const Search = React.memo((props: SearchPropsType) => {
+const Search = (props: SearchPropsType) => {
     const dispatch = useAppDispatch()
     const [value, setValue] = useState(props.value)
 
-
+    const onKeyUpHandler = useDebounce(() => props.onKeyUpHandler(value), 1000)
     const onEnterPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.charCode === 13) {
             props.onKeyUpHandler(value)
@@ -34,10 +35,11 @@ const Search = React.memo((props: SearchPropsType) => {
                value={value}
                placeholder={'Search...'}
                onChange={setInputValueHandler}
+               onKeyUp={onKeyUpHandler}
                onKeyPress={onEnterPressHandler}
         />
 
     )
-});
+};
 
 export default Search;
